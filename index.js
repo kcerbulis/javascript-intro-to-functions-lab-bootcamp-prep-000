58b75e27e9371983c8cfa9aff05baa56d13f4747
reactive-queue-1834 <kcerbulis@gmail.com>
2019-02-05 23:07:51 +0000

    Automatically backed up by Learn

--- index.js
+++ index.js
@@ -15,8 +15,8 @@ function logWhisper(string){
 }
 
 function sayHiToGrandma(string){
-  if string.toLowerCase() === string
-    return "I can't hear you!"
-  
+  if string.toLowerCase() === string {
+      return "I can't hear you!"
+  }
 }
 
